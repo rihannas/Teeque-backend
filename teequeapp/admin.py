8e849cb2d68b4ee47e511f6dcd139e6024b84c2c
rihanas <rihanna.socials@gmail.com>
2024-09-01 17:33:04 +0300

    registered classes for the admin site

--- teequeapp/admin.py
+++ teequeapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Seller)
+admin.site.register(Buyer)
+admin.site.register(Service)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Rating)
+admin.site.register(CustomUser)
+
+
