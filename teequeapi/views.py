a05ee6af9c19f83de632fd297a29a2a2e83a8251
rihanas <rihanna.socials@gmail.com>
2024-09-04 01:16:26 +0300

    add delete method for service viewset

--- teequeapi/views.py
+++ teequeapi/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from rest_framework import mixins, viewsets
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import status
+from rest_framework import mixins, viewsets
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
+
 from teequeapp.models import *
 from .serializers import *
 from .filters import *
@@ -18,6 +22,13 @@ class ServiceViewSet(viewsets.ModelViewSet):
     filterset_class = ServiceFilter
     search_fields = ['title', 'description']
     ordering_fields = ['price'] #there should be ordering by ratings too
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Service, pk=pk)
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Service cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class SellerViewSet(viewsets.ModelViewSet):
