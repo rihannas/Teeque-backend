3d4135edb7e840738d5e3546489f409e06d1741e
rihanas <rihanna.socials@gmail.com>
2024-09-03 22:36:48 +0300

    added urls + nested urls for sellers

--- teequeapi/urls.py
+++ teequeapi/urls.py
@@ -5,10 +5,16 @@ from .views import *
 
 router = DefaultRouter()
 router.register(r'services', ServiceViewSet, basename='services')
+router.register(r'sellers', SellerViewSet, basename='sellers')
+
 
 # Nested routers
-services_router = NestedSimpleRouter(router, r'services', lookup='service')
-services_router.register(r'services',  ServiceViewSet, basename='services')
+service_router = NestedSimpleRouter(router, r'services', lookup='service')
+service_router.register(r'services',  ServiceViewSet, basename='services')
+
+seller_router = NestedSimpleRouter(router, r'sellers', lookup='seller')
+seller_router.register(r'seller',  SellerViewSet, basename='sellers')
+
 
 urlpatterns = [
   path('', include(router.urls))
