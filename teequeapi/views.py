3ee47b2e5265c9a360f4d1bcd78385cab13cd4c4
rihanas <rihanna.socials@gmail.com>
2024-09-04 01:00:10 +0300

    add filters, ordering and searching for service viewset

--- teequeapi/views.py
+++ teequeapi/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework import mixins, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from teequeapp.models import *
 from .serializers import *
+from .filters import *
+
 
 # Create your views here.
 class ServiceViewSet(viewsets.ModelViewSet):
@@ -10,8 +14,14 @@ class ServiceViewSet(viewsets.ModelViewSet):
     """
     queryset = Service.objects.select_related('category_id', 'seller_id').prefetch_related('tags').all()
     serializer_class = ServiceSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ServiceFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['price'] #there should be ordering by ratings too
+
 
 class SellerViewSet(viewsets.ModelViewSet):
     queryset = Seller.objects.select_related('user').all()
     serializer_class = SellerSerializer
+
     lookup_field = 'id'
