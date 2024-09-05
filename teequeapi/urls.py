2bdaa0ee2a9dd8da25eb68b0e4d08020384b8a96
rihanas <rihanna.socials@gmail.com>
2024-09-06 02:14:58 +0300

    fixed errors + added nested router for reviews

--- teequeapi/urls.py
+++ teequeapi/urls.py
@@ -1,22 +1,18 @@
 from django.urls import path, include
 from rest_framework_nested.routers import DefaultRouter, NestedSimpleRouter
-
 from .views import *
 
+# Main router
 router = DefaultRouter()
 router.register(r'services', ServiceViewSet, basename='services')
 router.register(r'sellers', SellerViewSet, basename='sellers')
 router.register(r'buyers', BuyerViewSet, basename='buyers')
 
-
 # Nested routers
 service_router = NestedSimpleRouter(router, r'services', lookup='service')
-service_router.register(r'services',  ServiceViewSet, basename='services')
+service_router.register(r'reviews', RatingViewSet, basename='service-reviews')
 
 seller_router = NestedSimpleRouter(router, r'sellers', lookup='seller')
-seller_router.register(r'seller',  SellerViewSet, basename='sellers')
+seller_router.register(r'services', ServiceViewSet, basename='seller-services')
 
-
-urlpatterns = [
-  path('', include(router.urls))
-]  
+urlpatterns = router.urls + service_router.urls + seller_router.urls
