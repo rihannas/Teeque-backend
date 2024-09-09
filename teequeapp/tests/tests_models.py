eb6215f7029f8f10eb5eacae4a85f4e2b22a227d
rihanas <rihanna.socials@gmail.com>
2024-09-09 17:37:49 +0300

    added test class for buyer model

--- teequeapp/tests/tests_models.py
+++ teequeapp/tests/tests_models.py
@@ -83,4 +83,50 @@ class TestSellerModel(TestCase):
         seller_group = Group.objects.get(name='Seller') 
         self.assertEqual(self.seller.user.groups.get(name='Seller'), seller_group)
 
+class TestSellerModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = CustomUser.objects.create_user(email='sun@email.com', username='sun', password='ilovedjango', about='ilovesun', phonenumber='+12345678900')
+        seller = Seller(user=user)
+        seller.save()       
+    def setUp(self):
+        self.seller = Seller.objects.get(pk=1)
     
+    def test_str_method(self):
+        self.assertEqual(self.seller.__str__(), 'sun')
+
+    def test_seller_is_saved_to_seller_group(self):
+        seller_group = Group.objects.get(name='Seller') 
+        self.assertEqual(self.seller.user.groups.get(name='Seller'), seller_group)
+
+class TestSellerModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = CustomUser.objects.create_user(email='sun@email.com', username='sun', password='ilovedjango', about='ilovesun', phonenumber='+12345678900')
+        seller = Seller(user=user)
+        seller.save()       
+    def setUp(self):
+        self.seller = Seller.objects.get(pk=1)
+    
+    def test_str_method(self):
+        self.assertEqual(self.seller.__str__(), 'sun')
+
+    def test_seller_is_saved_to_seller_group(self):
+        seller_group = Group.objects.get(name='Seller') 
+        self.assertEqual(self.seller.user.groups.get(name='Seller'), seller_group)
+
+class TestBuyerModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = CustomUser.objects.create_user(email='sun@email.com', username='sun', password='ilovedjango', about='ilovesun', phonenumber='+12345678900')
+        buyer = Buyer(user=user)
+        buyer.save()       
+    def setUp(self):
+        self.buyer = Buyer.objects.get(pk=1)
+    
+    def test_str_method(self):
+        self.assertEqual(self.buyer.__str__(), 'sun')
+
+    def test_buyer_is_saved_to_buyer_group(self):
+        buyer_group = Group.objects.get(name='Buyer') 
+        self.assertEqual(self.buyer.user.groups.get(name='Buyer'), buyer_group)
