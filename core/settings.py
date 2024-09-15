e52fb740e0679ede38517691b8729e8a096eed18
rihanas <rihanna.socials@gmail.com>
2024-09-15 04:13:08 +0300

    update core files for browserable login to work

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,7 @@ AUTH_USER_MODEL = 'teequeapp.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     )
 }
 
--- core/urls.py
+++ core/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('teequeapi.urls')),
     path('', include('teequeapp.urls')),
+    path("api-auth/", include("rest_framework.urls")),  
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
