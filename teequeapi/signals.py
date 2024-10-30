759cd9bde2501d73d53936b43e902e16d3b61861
rihanas <rihanna.socials@gmail.com>
2024-10-31 01:45:32 +0300

    add signal which gets triggered when a new customuser isntance is created

---
+++ teequeapi/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import CustomUser, RegistrationProgress
+
+@receiver(post_save, sender=CustomUser)
+def create_registration_progress(sender, instance, created, **kwargs):
+    """Create RegistrationProgress instance when a new user is created"""
+    if created:
+        RegistrationProgress.objects.create(user=instance)
