8efa5587563e487f22905493400e5b21c131dc1e
rihanas <rihanna.socials@gmail.com>
2024-11-03 04:41:20 +0300

    adding urls

--- teequeapi/urls.py
+++ teequeapi/urls.py
@@ -10,6 +10,10 @@ router = DefaultRouter()
 router.register(r'services', ServiceViewSet, basename='services')
 router.register(r'sellers', SellerViewSet, basename='sellers')
 router.register(r'buyers', BuyerViewSet, basename='buyers')
+router.register('registration/progress', RegistrationProgressViewSet, basename='registration-progress')
+router.register('registration/profile', ProfileViewSet, basename='registration-profile')
+router.register('registration/type', UserTypeViewSet, basename='registration-type')
+
 
 # Nested routers
 service_router = NestedSimpleRouter(router, r'services', lookup='service')
@@ -18,4 +22,6 @@ service_router.register(r'reviews', RatingViewSet, basename='service-reviews')
 seller_router = NestedSimpleRouter(router, r'sellers', lookup='seller')
 seller_router.register(r'services', ServiceViewSet, basename='seller-services')
 
-urlpatterns = router.urls + service_router.urls + seller_router.urls
+
+
+urlpatterns = router.urls + service_router.urls + seller_router.urls 
